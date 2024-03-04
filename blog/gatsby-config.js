889d1b774f3e61d083514df1afda79745d05d8a4
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-03-04 10:32:10 -0600

    Completed SetUp-Project-1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Travel blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@CarmonaL`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Catarino Carmona`,
+      company: `Travel blog`,
+      address: `PO Boc 987`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
